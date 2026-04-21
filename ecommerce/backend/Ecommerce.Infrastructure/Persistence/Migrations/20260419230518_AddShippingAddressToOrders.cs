bc5987881be863e5f27c2ab0dc6aea6eb423f468
Franco Sassi <franco.sassi@brio.com.ar>
2026-04-20 23:32:27 -0300

    fase 6 error

--- ecommerce/backend/Ecommerce.Infrastructure/Persistence/Migrations/20260419230518_AddShippingAddressToOrders.cs
+++ ecommerce/backend/Ecommerce.Infrastructure/Persistence/Migrations/20260419230518_AddShippingAddressToOrders.cs
@@ -75,11 +75,10 @@ namespace Ecommerce.Infrastructure.Persistence.Migrations
                 defaultValue: "");
 
             migrationBuilder.AddColumn<Guid>(
-                name: "UserId",
-                table: "Orders",
-                type: "uuid",
-                nullable: false,
-                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+     name: "UserId",
+     table: "Orders",
+     type: "uuid",
+     nullable: true);
 
             migrationBuilder.AlterColumn<decimal>(
                 name: "UnitPrice",
